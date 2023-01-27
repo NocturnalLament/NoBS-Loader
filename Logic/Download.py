77bdca6b0db2571880a16e3331272885111f160a
Bradley Allen <103772876+NocturnalLament@users.noreply.github.com>
2023-01-27 15:42:52 -0500

    Added FFMPEG conversion.

--- Logic/Download.py
+++ Logic/Download.py
@@ -1,17 +1,28 @@
+import asyncio
 from pytube import YouTube
 from pathlib import Path
 import os
+import subprocess
+
+# So I remember this
+# SWAP_FORMAT_STRING = "ffmpeg -i example_initial_file.mp4 -vn -ar 44100 -ac 2 -b:a 129k output_file.mp3"
+ADDITIONAL_PATH = "\\DATA\\Audio"
 
 DOWNLOAD_AUDIO_PATH = os.path.dirname('./DATA')
 def downloadAudio(YouTubeObj, name):
     print('Downloading...')
     userPath = Path('Download.py').parent.resolve()
-    userPath = str(userPath) + "\\DATA\\Audio"
-    out_stream = YouTubeObj.streams.filter(progressive=True, file_extension='mp4').first()
-    outPutFile = out_stream.download(output_path=userPath, filename=name)
-    outPutFileText = Path(outPutFile)
-    #splits the text from the old extension to the new
-    base = os.path.splitext(outPutFileText)[0] + ".mp3"
-    os.rename(outPutFileText, base)
-    
+    userPath = str(userPath) + ADDITIONAL_PATH
+    download_name = "download_" + name + '.mp4'
+    out_stream = YouTubeObj.streams.filter(progressive=True, file_extension='mp4').first().download(output_path=userPath, filename=download_name)
+    fileString = name + '.mp3'
+    out_path = str(Path(out_stream))
+    return out_path
+
+def convertAudio(in_name, out_name):
+    print("Converting your audio")
+    path = str(Path(f'{in_name}.mp4').parent.resolve()) + ADDITIONAL_PATH
+    command = f'ffmpeg -i {in_name} -vn {out_name}'
+    os.system(f'cd {path} && {command}')
+  
     
--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import Logic.Queries.BasicQuery
 import Logic.Download
 import os
 from pathlib import Path
-
+import asyncio
 
 ytQuery = Logic.Queries.BasicQuery
 ytDownload = Logic.Download
 userYTPath = ytQuery.basicQuery()
 # getUserStream = ytQuery.findStream(userYTPath)
 newVideoName = input("Please enter a name for the downloaded file: ")
-downloadVideo = ytDownload.downloadAudio(userYTPath, newVideoName)
+#downloadVideo = ytDownload.downloadAudio(userYTPath, newVideoName)
+download_video_name = f'download_{newVideoName}.mp4'
+output_video_name = f'{newVideoName}.mp4'
+mp4Input = ytDownload.downloadAudio(userYTPath, newVideoName)
+ytDownload.convertAudio(download_video_name, output_video_name)
+
+
