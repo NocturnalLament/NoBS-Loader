e44964e02d01a62e69f949c26cc79f0cf8f2b73f
Bradley Allen <103772876+NocturnalLament@users.noreply.github.com>
2023-01-28 18:12:18 -0500

    Added user selection to choose between mp3 and mp4, and split up the logic.  More dialogue was added to inform where the user where the program is.

--- Logic/Download.py
+++ Logic/Download.py
@@ -1,28 +1,50 @@
-import asyncio
 from pytube import YouTube
 from pathlib import Path
 import os
-import subprocess
+
 
 # So I remember this
 # SWAP_FORMAT_STRING = "ffmpeg -i example_initial_file.mp4 -vn -ar 44100 -ac 2 -b:a 129k output_file.mp3"
-ADDITIONAL_PATH = "\\DATA\\Audio"
-
+ADDITIONAL_PATH = "\\DATA"
 DOWNLOAD_AUDIO_PATH = os.path.dirname('./DATA')
-def downloadAudio(YouTubeObj, name):
-    print('Downloading...')
-    userPath = Path('Download.py').parent.resolve()
-    userPath = str(userPath) + ADDITIONAL_PATH
-    download_name = "download_" + name + '.mp4'
-    out_stream = YouTubeObj.streams.filter(progressive=True, file_extension='mp4').first().download(output_path=userPath, filename=download_name)
-    fileString = name + '.mp3'
-    out_path = str(Path(out_stream))
-    return out_path
+# def downloadAudio(YouTubeObj, name):
+#     print('Downloading...')
+#     userPath = Path('Download.py').parent.resolve()
+#     userPath = str(userPath) + ADDITIONAL_PATH
+#     download_name = "download_" + name + '.mp4'
+#     out_stream = YouTubeObj.streams.filter(progressive=True, file_extension='mp4').first().download(output_path=userPath, filename=download_name)
+#     fileString = name + '.mp3'
+#     out_path = str(Path(out_stream))
+#     return out_path
 
-def convertAudio(in_name, out_name):
-    print("Converting your audio")
-    path = str(Path(f'{in_name}.mp4').parent.resolve()) + ADDITIONAL_PATH
-    command = f'ffmpeg -i {in_name} -vn {out_name}'
-    os.system(f'cd {path} && {command}')
+# def convertAudio(in_name, out_name):
+#     print("Converting your audio")
+#     path = str(Path(f'{in_name}.mp4').parent.resolve()) + ADDITIONAL_PATH
+#     command = f'ffmpeg -i {in_name} -vn {out_name}'
+#     os.system(f'cd {path} && {command}')
   
-    
+def get_user_audio(url, name, success):
+    print("Downloading....")
+    YouTube_Stream = YouTube(url)
+    YouTube_Stream = YouTube_Stream.streams.filter(only_audio=True).first()
+    file_name = name.replace(" ", "_")
+    success_name = success.replace(" ", "_") + '.mp3'
+    userPath = Path('Download.py').parent.resolve()
+#     userPath = str(userPath) + ADDITIONAL_PATH
+    path_string = str(userPath) + ADDITIONAL_PATH + '\\Audio'
+    YouTube_File = YouTube_Stream.download(output_path=path_string, filename=file_name)
+    base, ext = os.path.splitext(YouTube_File)
+    new_file = base + '.mp3'
+    os.rename(YouTube_File, new_file)
+    print(f'Successfully downloaded {success_name}')
+
+def get_user_video(url, name, success):
+    print("Finding video...")
+    YouTube_Stream = YouTube(url=url)
+    YouTube_Stream = YouTube_Stream.streams.get_highest_resolution()
+    file_name = name.replace(" ", "_")
+    print("Downloading...")
+    path_parent= Path('Download.py').parent.resolve()
+    path_string = str(path_parent) + ADDITIONAL_PATH + '\\VIDEO'
+    YouTube_Stream.download(output_path=path_string, filename=file_name)
+    print(f'Successfully downloaded {success}')
--- main.py
+++ main.py
@@ -3,16 +3,52 @@ import Logic.Download
 import os
 from pathlib import Path
 import asyncio
+import inquirer
 
 ytQuery = Logic.Queries.BasicQuery
 ytDownload = Logic.Download
-userYTPath = ytQuery.basicQuery()
-# getUserStream = ytQuery.findStream(userYTPath)
-newVideoName = input("Please enter a name for the downloaded file: ")
-#downloadVideo = ytDownload.downloadAudio(userYTPath, newVideoName)
-download_video_name = f'download_{newVideoName}.mp4'
-output_video_name = f'{newVideoName}.mp4'
-mp4Input = ytDownload.downloadAudio(userYTPath, newVideoName)
-ytDownload.convertAudio(download_video_name, output_video_name)
+#userYTPath = ytQuery.basicQuery()
+
+def file_select():
+    questions = [inquirer.List('media_type', message="What media type do you need?",
+                                choices=["MP3", "MP4"])]
+    response = inquirer.prompt(questions)
+    response = response.get('media_type')
+    return response
+
+def download_audio_setup():
+    userYoutubeLinkInput = input("Please enter the URL to your video: ")
+    new_video_name = input("Please enter a video name: ")
+    output_video_name = f'{new_video_name}.mp4'
+    success_name = new_video_name
+    ytDownload.get_user_audio(userYoutubeLinkInput, output_video_name, success_name)
+
+def download_video_setup():
+    user_youtube_link = input("Please enter the URL to your video: ")
+    new_video_name = input("Please enter a name: ")
+    output_video_name = f'{new_video_name}.mp4'
+    success_name = new_video_name
+    ytDownload.get_user_video(user_youtube_link, output_video_name, success_name)
 
 
+# to hold the switch statement
+def user_selection_logic(prompt_response):
+    if prompt_response == "MP3":
+        download_audio_setup()
+    elif prompt_response == "MP4":
+        download_video_setup()
+
+if __name__ == "__main__":
+    prompt = file_select()
+    user_selection_logic(prompt_response=prompt)
+        
+# userYoutubeLinkInput = input("Please enter the URL to your video: ")
+# # getUserStream = ytQuery.findStream(userYTPath)
+# newVideoName = input("Please enter a name for the downloaded file: ")
+# #downloadVideo = ytDownload.downloadAudio(userYTPath, newVideoName)
+# download_video_name = f'download_{newVideoName}.mp4'
+# output_video_name = f'{newVideoName}.mp4'
+# success_name = newVideoName
+# #mp4Input = ytDownload.downloadAudio(userYTPath, newVideoName)
+# #ytDownload.convertAudio(download_video_name, output_video_name)
+# ytDownload.get_user_audio(userYoutubeLinkInput, output_video_name, success_name
