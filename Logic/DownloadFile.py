b189f92142ac3b786ccf71d5c4c3ec4aae2e5501
Bradley Allen <103772876+NocturnalLament@users.noreply.github.com>
2023-01-30 18:28:39 -0500

    Removed a file for clarity

--- Logic/DownloadFile.py
+++ Logic/DownloadFile.py
@@ -1,7 +1,8 @@
-class AudioFile:
-    def __init__(self, name, download_name, url, description, length):
+class DownloadFileClass:
+    def __init__(self, name, download_name, url, description, type, length):
         self.name = name
         self.download_name = download_name
         self.url = url
         self.description = description
+        self.type = type
         self.length = length
--- Logic/Queries/BasicQuery.py
+++ Logic/Queries/BasicQuery.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
-from Logic import VideoClass
+from Logic import DownloadFile
 #returns some basic information
-download_classes = VideoClass
+download_classes = DownloadFile
 def basicQuery():
     userYoutubeLinkInput = input("Please enter the URL to your video: ")
     userYoutubeLink = YouTube(userYoutubeLinkInput)
@@ -23,7 +23,7 @@ def add_data_to_db(url, name):
     video_description = video_data.description
     video_url = url
     video_length = video_data.length
-    vid = download_classes.AudioFile(name=video_title, download_name=video_download_name, 
+    vid = download_classes.DownloadFileClass(name=video_title, download_name=video_download_name, 
                                                     url=video_url, description=video_description, 
                                                         length=video_length)
     print(vid.download_name)
